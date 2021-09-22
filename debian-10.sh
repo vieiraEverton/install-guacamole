ee96617f82e99d1d34004c940053582bea65a63a
Everton Vieira <everton.vieira@venturus.org.br>
2021-09-22 15:07:06 -0300

    install cmake

--- debian-10.sh
+++ debian-10.sh
@@ -10,7 +10,7 @@ apt install -y tomcat9 tomcat9-admin tomcat9-common tomcat9-user
 echo
 echo "Installing required packages"
 echo
-apt install build-essential libcairo2-dev libjpeg62-turbo-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libwebsockets-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev  libsystemd-dev libsdl2-dev libgtk2.0-dev libavcodec-dev libavformat-dev libavfilter-dev liblzo2-dev libgnutls28-dev libpng-dev libvncserver-dev libjpeg-dev libgcrypt20-dev zlib1g-dev
+apt install -y build-essential libcairo2-dev libjpeg62-turbo-dev libtool-bin libossp-uuid-dev libavcodec-dev libavutil-dev libswscale-dev freerdp2-dev libpango1.0-dev libssh2-1-dev libtelnet-dev libvncserver-dev libwebsockets-dev libpulse-dev libssl-dev libvorbis-dev libwebp-dev  libsystemd-dev libsdl2-dev libgtk2.0-dev libavcodec-dev libavformat-dev libavfilter-dev liblzo2-dev libgnutls28-dev libpng-dev libvncserver-dev libjpeg-dev libgcrypt20-dev zlib1g-dev
 echo
 echo "Install cmake"
 echo
